4580df98127ff22075de48555178b08ec944caf0
szchenghuang <szchenghuang@gmail.com>
2016-05-21 16:55:57 +0800

    Initialization.

---
+++ src/index.js
@@ -0,0 +1,90 @@
+(function() {
+    "use strict";
+
+    function AE( config ) {
+        this.className  = config && config.className  || 'animated-ellipsis';
+        this.style      = config && config.style      || undefined;
+        this.spacing    = config && config.spacing    || '1rem';
+        this.fontSize   = config && config.fontSize   || '2rem';
+        this.marginLeft = config && config.marginLeft || '1rem';
+        this.steps      = 3; 
+        this.duration   = 2;
+        this.perDelay   = 0.2;
+
+        this.insertStyleNode();
+    }
+
+    AE.prototype.insertStyleNode = function() {
+        const style = document.createElement( 'style' );
+        style.type = 'text/css';
+        style.innerHTML = ' \
+            @-webkit-keyframes animated-ellipsis { \
+                0%   { opacity: 1; } \
+                50%  { opacity: 0; } \
+                100% { opacity: 1; } \
+            } \
+            @keyframes animated-ellipsis { \
+                0%   { opacity: 1; } \
+                50%  { opacity: 0; } \
+                100% { opacity: 1; } \
+            }';
+        this.style = document.getElementsByTagName( 'head' )[ 0 ].appendChild( style );
+    }
+
+    function animate( elem ) {
+        var wrapper = elem;
+        const steps = AnimatedEllipsis.steps;
+        const perDelay = AnimatedEllipsis.perDelay;
+        const marginLeft = wrapper.dataset.marginLeft || AnimatedEllipsis.marginLeft;
+        const spacing = wrapper.dataset.spacing || AnimatedEllipsis.spacing;
+
+        wrapper.style.marginLeft = marginLeft;
+
+        for ( var iStep = 0; iStep < steps; ++iStep ) {
+            var elemSpan = document.createElement( 'span' );
+            const delay = iStep * perDelay;
+
+            elemSpan.className = '.$step' + iStep;
+            elemSpan.innerHTML = '.';
+            var cssText = '\
+                opacity: 0; \
+                font-size: ' + AnimatedEllipsis.fontSize + '; \
+                -webkit-animation: animated-ellipsis ' + AnimatedEllipsis.duration + 's infinite; \
+                animation: animated-ellipsis ' + AnimatedEllipsis.duration + 's infinite; \
+                -webkit-animation-delay: ' + delay+ 's; \
+                animation-delay: ' + delay + 's; \
+            ';
+
+            if ( iStep !== 0 ) {
+                cssText += ( 'margin-left: ' + spacing + ';' );
+            }
+
+            elemSpan.style.cssText = cssText;
+            elemSpan = wrapper.appendChild( elemSpan );
+        }
+    }
+
+    window.HTMLElement.prototype.animateEllipsis = function() {
+        animate( this );
+    };
+
+    window.NodeList.prototype.animateEllipsis = function() {
+        for ( elem of this ) {
+            animate( elem );
+        }
+    }
+
+    window.HTMLElement.prototype.stopAnimatingEllipsis = function() {
+        var wrapper = this;
+        while ( wrapper.hasChildNodes() ) {
+            wrapper.removeChild( wrapper.lastChild );
+        }
+
+        delete wrapper.style.marginLeft;
+    };
+
+    AnimatedEllipsis = new AE();
+
+    module.exports = AnimatedEllipsis;
+
+}());
