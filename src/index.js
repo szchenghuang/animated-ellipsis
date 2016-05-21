f11b5b62e5d232b7fbfa6c8b10d09d6abccbf2a4
szchenghuang <szchenghuang@gmail.com>
2016-05-21 17:59:25 +0800

    Display the animation vividly.

--- src/index.js
+++ src/index.js
@@ -19,12 +19,12 @@
         style.type = 'text/css';
         style.innerHTML = ' \
             @-webkit-keyframes animated-ellipsis { \
-                0%   { opacity: 1; } \
+                0%   { opacity: 0; } \
                 50%  { opacity: 0; } \
                 100% { opacity: 1; } \
             } \
             @keyframes animated-ellipsis { \
-                0%   { opacity: 1; } \
+                0%   { opacity: 0; } \
                 50%  { opacity: 0; } \
                 100% { opacity: 1; } \
             }';
